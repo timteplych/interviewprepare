5a614a14ce032bff804b6d99f7d18da4d27c8703
timteplych <timteplych@mail.ru>
2019-08-25 10:18:34 +0800

    Initial commit

---
+++ src/main/java/ru/ttv/interviewprepare/App.java
@@ -0,0 +1,13 @@
+package ru.ttv.interviewprepare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
